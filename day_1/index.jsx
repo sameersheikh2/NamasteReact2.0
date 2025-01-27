4c13782a7217fac533d4b3fdfe4318df12fb92df
sameersheikh2 <sameersheikh819@gmail.com>
2025-01-27 17:09:30 +0530

    day 1 of learning

---
+++ day_1/index.jsx
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", {}, "Hello world from ReactJS");
+
+const parent = React.createElement("div", { id: "parent" }, heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
