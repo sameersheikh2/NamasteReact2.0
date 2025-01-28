bedf94889a713dd533509f35575156c324840489
sameersheikh2 <sameersheikh819@gmail.com>
2025-01-28 20:13:09 +0530

    init plus added parcel

---
+++ day_2/dist/index.3f36e024.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1", {}, "Hello world from ReactJS");
+const parent = React.createElement("div", {
+    id: "parent"
+}, heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.3f36e024.js.map
