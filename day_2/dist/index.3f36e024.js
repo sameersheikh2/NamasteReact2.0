e84896625dea133b1ee4c1ebae442e9869ead892
sameersheikh2 <sameersheikh819@gmail.com>
2025-01-30 20:29:39 +0530

    allow empty

--- day_2/dist/index.3f36e024.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement("h1", {}, "Hello world from ReactJS");
-const parent = React.createElement("div", {
-    id: "parent"
-}, heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
-//# sourceMappingURL=index.3f36e024.js.map
